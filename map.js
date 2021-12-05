d9c15d8e6087f0e4abaea19718930f8460f1a9ae
danielex1999 <daniel_jul_can@hotmail.com>
2021-12-05 00:45:38 -0500

    Visualize icons and buttons

--- map.js
+++ map.js
@@ -1,17 +1,34 @@
 //Variables
 
-let reservar="localhost:8080/ReserveService/Add"
+let reservar = "localhost:8080/ReserveService/Add";
 let prueba = "http://localhost:8080/Parkinglocal/Showall";
 let locales = [];
-const welcome= document.querySelector('.welcome')
-const reserve= document.querySelector('.reserve-data')
-const name_parking=document.querySelector('.title h1')
-const desc_parking=document.querySelector('.description h1')
+const welcome = document.querySelector(".welcome");
+const reserve = document.querySelector(".reserve-data");
+const name_parking = document.querySelector(".title h1");
+const desc_parking = document.querySelector(".description h1");
+const tipvehicle = document.querySelector(".containtext h2");
+const image_vehicle = document.querySelector(".imagvehicle");
+const price = document.querySelector(".price_card h2");
+const total = document.querySelector(".totalprice h2");
+let inicial = document.querySelector(".fechaentrada input");
+let seconial = document.querySelector(".fechasalida input");
+let preciobase=1;
+let preciobasefinal=1;
 
 
-
+inicial.valueAsDate = new Date();
+seconial.valueAsDate = new Date();
+//Resta de dias
+inicial.valueAsDate = new Date();
+inicial.onchange = function () {};
+seconial.onchange = function () {
+  var second = new Date(this.value);
+  var first = new Date(inicial.value);
+  preciobasefinal=(preciobase*(second.getTime()-first.getTime())/(3600*1000*24))
+  total.innerHTML= (preciobase*(second.getTime()-first.getTime())/(3600*1000*24))+" €"
+};
 //functions
-
 function initMap() {
   var options = {
     zoom: 16,
@@ -43,11 +60,17 @@ function initMap() {
         });
         (function (marker, locales) {
           google.maps.event.addListener(marker, "click", function (e) {
-            console.log(locales[index].description)
-            welcome.style.display='none'
-            reserve.style.display='flex'
-            name_parking.innerHTML = locales[index].name
-            desc_parking.innerHTML = locales[index].description
+            console.log(locales[index].description);
+            welcome.style.display = "none";
+            reserve.style.display = "flex";
+            name_parking.innerHTML = locales[index].name;
+            desc_parking.innerHTML = locales[index].description;
+            tipvehicle.innerHTML =
+              locales[index].catalogservices[0].type_service;
+            document.getElementById("imagvehicle").src =
+              "img/" + locales[index].catalogservices[0].type_service + ".png";
+            price.innerHTML = locales[index].catalogservices[0].price + " €";
+            preciobase = locales[index].catalogservices[0].price;
           });
         })(marker, locales);
       }
