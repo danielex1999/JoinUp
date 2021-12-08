d3919290ebf75e858c02b794ec3643d4f27deebd
danielex1999 <daniel_jul_can@hotmail.com>
2021-12-08 18:43:19 -0500

    final

--- map.js
+++ map.js
@@ -1,6 +1,5 @@
 //Variables
-
-let reservar = "localhost:8080/ReserveService/Add";
+let reservar = "http://localhost:8080/ReserveService/Add";
 let prueba = "http://localhost:8080/Parkinglocal/Showall";
 let locales = [];
 const welcome = document.querySelector(".welcome");
@@ -13,21 +12,57 @@ const price = document.querySelector(".price_card h2");
 const total = document.querySelector(".totalprice h2");
 let inicial = document.querySelector(".fechaentrada input");
 let seconial = document.querySelector(".fechasalida input");
-let preciobase=1;
-let preciobasefinal=1;
-
-
-inicial.valueAsDate = new Date();
-seconial.valueAsDate = new Date();
+const button = document.querySelector(".reservebutton button");
+let preciobase = 1;
+let preciobasefinal = 1;
+/*marca, modelo, placa*/
+const marca = document.querySelector(".marca input");
+const modelo = document.querySelector(".modelo input");
+const placa = document.querySelector(".placa input");
 //Resta de dias
 inicial.valueAsDate = new Date();
-inicial.onchange = function () {};
+seconial.valueAsDate = new Date();
 seconial.onchange = function () {
   var second = new Date(this.value);
   var first = new Date(inicial.value);
-  preciobasefinal=(preciobase*(second.getTime()-first.getTime())/(3600*1000*24))
-  total.innerHTML= (preciobase*(second.getTime()-first.getTime())/(3600*1000*24))+" €"
+  preciobasefinal =
+    (preciobase * (second.getTime() - first.getTime())) / (3600 * 1000 * 24);
+  console.log(first.getTime());
+  total.innerHTML =
+    (preciobase * (second.getTime() - first.getTime())) / (3600 * 1000 * 24) +
+    " €";
 };
+//POST
+button.addEventListener("click", () => {
+  const generarPost = {
+    /*fecha de inicio,fecha de salida, marca, modelo,placa, precio*/
+    reserva_id: 5,
+    start_day: inicial.value,
+    final_day: seconial.value,
+    brand: marca.value,
+    model: modelo.value,
+    placa: placa.value,
+    money: preciobasefinal,
+    user_id: {
+      id: "1",
+      lastname: "Cavero Tovar",
+      mail: "1632116@utp.edu.pe",
+      name: "Juan Daniel",
+      number: "989296287",
+    },
+  };
+  console.log(generarPost);
+  fetch(reservar, {
+    method: "POST",
+    body: JSON.stringify(generarPost),
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((datos) => console.log(datos));
+});
+
 //functions
 function initMap() {
   var options = {
@@ -36,7 +71,7 @@ function initMap() {
     disableDefaultUI: true,
   };
   var map = new google.maps.Map(document.getElementById("map"), options);
-
+  //Añadicion de coordenadas
   function addMarker(cordenadas) {
     var marker = new google.maps.Marker({
       position: cordenadas,
@@ -60,7 +95,6 @@ function initMap() {
         });
         (function (marker, locales) {
           google.maps.event.addListener(marker, "click", function (e) {
-            console.log(locales[index].description);
             welcome.style.display = "none";
             reserve.style.display = "flex";
             name_parking.innerHTML = locales[index].name;
